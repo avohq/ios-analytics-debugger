45d60c33dd21ab04b528a638275ee86729ab9d0d
Alexey Verein <tpom6oh@gmail.com>
2020-03-12 20:17:06 +0100

    Remove commented code

--- IosAnalyticsDebugger/Classes/eventslistscreen/secondaryrows/EventTableViewSecondaryCell.m
+++ IosAnalyticsDebugger/Classes/eventslistscreen/secondaryrows/EventTableViewSecondaryCell.m
@@ -33,12 +33,8 @@
 }
 
 - (void) showError: (NSAttributedString *) errorMessage {
-  //  NSBundle *selfBundle = [NSBundle bundleForClass:self.class];
-    
-    [self.propName setTextColor:[UIColor colorWithRed:0.851 green:0.271 blue:0.325 alpha:1]];
-     //[UIColor colorNamed:@"error_color" inBundle:selfBundle compatibleWithTraitCollection:nil]];
-    [self.propValue setTextColor:[UIColor colorWithRed:0.851 green:0.271 blue:0.325 alpha:1]];
-     //[UIColor colorNamed:@"error_color" inBundle:selfBundle compatibleWithTraitCollection:nil]];
+    [self.propName setTextColor:[UIColor colorWithRed:0.851 green:0.271 blue:0.325 alpha:1]]; // @"error_color"
+    [self.propValue setTextColor:[UIColor colorWithRed:0.851 green:0.271 blue:0.325 alpha:1]]; // @"error_color"
     
     [self.message setAttributedText:errorMessage];
 }
